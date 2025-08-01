d628ce82ea957f552c1558d78ffaeae6b66d4ef2
TanishJhamtani <26394188-TanishJhamtani@users.noreply.replit.com>
2025-08-01 05:45:57 +0000

    Restored to 'da7f1e000e26382255cac7b6f72159b23cff4d6c'
    Replit-Restored-To: da7f1e000e26382255cac7b6f72159b23cff4d6c

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import App from "../../src/App.jsx";
-import "../../src/index.css";
+import App from "./App";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
